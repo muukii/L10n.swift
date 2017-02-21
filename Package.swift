07ac4d39f9029fb9bcd8d8bfec1cadfc91b9d65a
muukii <m@muukii.me>
2017-02-21 16:46:49 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
   name: "l10n",
   dependencies: [
     .Package(url: "https://github.com/muukii/JAYSON.git", majorVersion: 0),
-    .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0),
     .Package(url: "git@github.com:kylef/Commander.git", majorVersion: 0),
   ]
 )

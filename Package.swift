f4a71630de077cf1c3af1a12686d018269b4f008
muukii <m@muukii.me>
2017-03-07 14:42:50 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     .Package(url: "https://github.com/muukii/JAYSON.git", majorVersion: 1),
     .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
     .Package(url: "https://github.com/oarrabi/Guaka", majorVersion: 0),
+    .Package(url: "https://github.com/oarrabi/Regex", majorVersion: 0),
     .Package(url: "https://github.com/kylef/Stencil.git", majorVersion: 0),
   ]
 )

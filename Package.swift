176780089702221a7713691a07e1857f4c280355
muukii <m@muukii.me>
2017-03-24 19:33:55 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     .Package(url: "https://github.com/muukii/JAYSON.git", majorVersion: 1),
     .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
     .Package(url: "https://github.com/oarrabi/Guaka", majorVersion: 0),
-    .Package(url: "https://github.com/oarrabi/Regex", majorVersion: 0),
     .Package(url: "https://github.com/ArtSabintsev/Guitar.git", majorVersion: 0),
   ]
 )

842943aa13f0bce8a881492027bc3c36d85b84a6
muukii <m@muukii.me>
2017-06-28 12:40:25 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
     .package(url: "https://github.com/kylef/Commander.git", from: "0.6.0"),
-    .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "0.0.12"),
+    .package(url: "https://github.com/ArtSabintsev/Guitar.git", .exact("0.0.12")),
   ],
   targets: [    
     .target(name: "L10n", dependencies: ["Guitar", "Commander"]),
